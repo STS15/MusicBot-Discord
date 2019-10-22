5c59c585a385cdd82bcc855eba3c1b59e92e2067
jjab2034 <38052370+jjab2034@users.noreply.github.com>
2019-10-21 22:07:39 -0700

    revert to version "fixes"

--- MusicBot.js
+++ MusicBot.js
@@ -115,43 +115,6 @@ Please provide a value to select one of the search results ranging from 1-10.
 		msg.voiceChannel.send("I'm gonna say the N-Word!!");
 
 	}
-
-	else if (command === 'cbt'){
-		if (!msg.member.voiceChannel) return msg.channel.send('I\'m gonna say the N-Word!!');
-		url="https://youtu.be/EbwRFcoEugQ"
-		try {
-				var video = await youtube.getVideo(url);
-			} catch (error) {
-				try {
-					var videos = await youtube.searchVideos(searchString, 10);
-					let index = 0;
-					msg.channel.send(`
-__**Song selection:**__
-${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
-Please provide a value to select one of the search results ranging from 1-10.
-					`);
-					// eslint-disable-next-line max-depth
-					try {
-						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-							maxMatches: 1,
-							time: 10000,
-							errors: ['time']
-						});
-					} catch (err) {
-						console.error(err);
-						return msg.channel.send('No or invalid value entered, cancelling video selection.');
-					}
-					const videoIndex = parseInt(response.first().content);
-					var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
-				} catch (err) {
-					console.error(err);
-					return msg.channel.send('I could not obtain any search results.');
-				}
-			}
-			return handleVideo(video, msg, voiceChannel);
-		}
-	}
-
 	//
 	
 	//This is stop
@@ -164,10 +127,10 @@ Please provide a value to select one of the search results ranging from 1-10.
 	} else if (command === 'volume') {
 		if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
 		if (!serverQueue) return msg.channel.send('There is nothing playing.');
-		if (!args[1]) return msg.channel.send('The current volume is:' **${serverQueue.volume}**);
+		if (!args[1]) return msg.channel.send(`The current volume is: **${serverQueue.volume}**`);
 		serverQueue.volume = args[1];
 		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 10);
-		return msg.channel.send('I set the volume to:' **${args[1]}**);
+		return msg.channel.send(`I set the volume to: **${args[1]}**`);
 	} else if (command === 'help') {
 		msg.channel.send('**WELCOME TO SHITTY MUSIC FROM JACOB!**\nUse **-** for commands.\n-help [This command]\n-play (url) [Plays the youtube video specified]\n-stop [Stops bot from making noise and removes from voice chat]\n-skip [Next song in queue]\n-next\n-volume (1-10) **Warning this can get really loud**\n-queue [Displays songs in queue]\n-pause\n-resume');
 	} else if (command === 'np') {
